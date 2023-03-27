049ff8bad62da26359a7866afad9c1e0d128fff7
Reslll <127844879+Reslll@users.noreply.github.com>
2023-03-27 19:01:09 -0400

    Js: Navbar menu click

--- main.js
+++ main.js
@@ -8,5 +8,20 @@ document.addEventListener('scroll', () => {
     navbar.classList.add('navbar--dark');
   else 
     navbar.classList.remove('navbar--dark');
-
 });
+
+
+//scroll to a specific place when nav is clicked
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  console.log(event.target.dataset.link);
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior:'smooth'});
+});
+
