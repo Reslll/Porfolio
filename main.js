a6c64218e0162253489e7ffb412d506d2ec77263
Reslll <127844879+Reslll@users.noreply.github.com>
2023-03-27 19:19:51 -0400

    Js: home transparent

--- main.js
+++ main.js
@@ -29,6 +29,18 @@ homeContactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
 })
 
+
+//decrease opacity as scroll down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=> {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+})
+
+
+
+
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({behavior:'smooth'})
